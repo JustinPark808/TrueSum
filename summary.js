dd0739db016dca9a12307edd578fa14b3cd14ae7
Justin <justin.park808@gmail.com>
2018-01-22 09:40:36 -0500

    Fixed indentation

--- summary.js
+++ summary.js
@@ -1,16 +1,16 @@
 // When the extension is loaded, get the current tab's Url and display its
 // summary in 'popup.html'.
 document.addEventListent('DOMContentLoaded', function() {
-	getCurrentTabUrl(function(url) {
-		renderText('Summarizing...');
+    getCurrentTabUrl(function(url) {
+        renderText('Summarizing...');
 
-		// Aylien API id and key
-		var id = 'a5e6d8fe';
-		var key = 'ce95fc592f8386c636e0abff43de1efa';
+        // Aylien API id and key
+        // var id = 'a5e6d8fe';
+        // var key = 'ce95fc592f8386c636e0abff43de1efa';
 
-		// Get and display the summary
-		displaySummary(url, id, key);
-	});
+        // Get and display the summary
+        // displaySummary(url, id, key);
+    });
 });
 
 /**
@@ -26,7 +26,7 @@ function getCurrentTabUrl(callback) {
  * Renders text in 'summary' in 'popup.html'.
  */
 functions renderText(text) {
-	document.getElementById('summary').innerText = text;
+    document.getElementById('summary').innerText = text;
 }
 
 /**
